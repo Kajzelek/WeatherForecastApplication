f8d2cc5250bd60619cf52c9c30b5ef08f8fb09a8
Patryk Cygnar <patryk.cygnar@student.pk.edu.pl>
2024-06-28 16:20:43 +0200

    WebConfig CorsMapping added

--- Backend/src/main/java/com/example/WeatherForecastApplication/Config/WebConfig.java
+++ Backend/src/main/java/com/example/WeatherForecastApplication/Config/WebConfig.java
@@ -1,17 +1,21 @@
 package com.example.WeatherForecastApplication.Config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig implements WebFluxConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
-                .allowedHeaders("Origin", "X-Requested-With", "Content-Type", "Accept");
+                .allowedMethods("*")
+                .allowedHeaders("*");
     }
 }
