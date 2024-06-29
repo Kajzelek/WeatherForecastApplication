ab70d2abe5f466c93b87924f0eb1349598cbcebc
Patryk Cygnar <patryk.cygnar@gmail.com>
2024-06-29 16:56:22 +0200

    FetchingData from API in FrontEnd

--- Frontend/front-app/src/App.tsx
+++ Frontend/front-app/src/App.tsx
@@ -1,11 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
+import { fetchWeatherData, formatDate } from './Functions/weatherApi';
 
 function App() {
+
+  interface WeatherForecast {
+    date: Date,
+    weatherCode: number,
+    minTemperature: number,
+    maxTemperature: number,
+  }
+  
+
+  const [weatherForecasts, setWeatherForecasts] = useState<WeatherForecast[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchWeatherData({latitude: 52.2297, longitude: 21.0122}); // Użyj rzeczywistych wartości dla szerokości i długości geograficznej
+        setWeatherForecasts(data);
+      } catch (error) {
+        console.error('Error fetching weather data:', error);
+      }
+    };
+
+    fetchData();
+    console.log(weatherForecasts)
+  }, []);
+
   return (
     <div className="App">
-      12345
+      {weatherForecasts.length > 0 ? (
+          <ul>
+            {weatherForecasts.map((forecast, index) => (
+              <li key={index}>
+                Data: {formatDate(new Date(forecast.date))}, TemperaturaMin: {forecast.minTemperature}°C, TemperaturaMax: {forecast.maxTemperature}°C, KodPogody: {forecast.weatherCode}
+              </li>
+            ))}
+          </ul>
+      ) : (
+        <p>Ładowanie danych pogodowych...</p>
+      )} 
     </div>
   );
 }
--- Frontend/front-app/src/Functions/weatherApi.ts
+++ Frontend/front-app/src/Functions/weatherApi.ts
@@ -1,7 +1,27 @@
+import axios, { AxiosResponse } from "axios"
+
 export interface WeatherForecast{
     date: Date,
     weatherCode: number,
-    minTemp: number,
-    maxTemp: number,
-    energy: number,
+    minTemperature: number,
+    maxTemperature: number,
 }
+
+export const fetchWeatherData = async (params: {latitude: number, longitude: number}): Promise<WeatherForecast[]> => {
+    try{
+        const response: AxiosResponse<WeatherForecast[]> = await axios.get(`http://localhost:8080/api/v1/forecast?latitude=${params.latitude}&longitude=${params.longitude}`);            
+        return response.data
+    }catch(error){
+        console.error(error)
+        throw new Error("Failed to fetch weather data")
+        return []
+    }
+}
+
+export const formatDate = (dateString: Date) => {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${day}-${month}-${year}`;
+};
